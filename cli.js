92e5cbfa15bfc0ed94cff2c09f7d78a9e0481b2e
twoheart <twoheart@vip.qq.com>
2020-09-09 11:05:56 +0800

    show all

--- cli.js
+++ cli.js
@@ -1,24 +1,27 @@
-const { Command } = require('commander');
+const { program } = require('commander');
 const api = require('./index.js');
-const program = new Command();
 
-program
-  .option('-x, --xxx', 'what the x')
 
 program
   .command('add')
   .description('add a task')
   .action((...args) => {
     const words = args.slice(1).join(' ');
-    api.add(words);
+    api.add(words).then(() => {console.log('添加成功')}).then(() => {console.log('添加失败')});
   });
 
 program
   .command('clear')
   .description('clear all task')
   .action(() => {
+    api.clear();
     console.log('clear all');
   });
 
 
+if (process.argv.length === 2) {
+  console.log('process');
+  // 说明用户没有传任何参数
+  api.showAll();
+}
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -8,3 +8,11 @@ module.exports.add = async (title) => {
   // 存储任务到文件
   await db.write(list);
 };
+
+module.exports.clear = async () => {
+  await db.write([]);
+}
+
+module.exports.showAll = async () => {
+  console.log('show all');
+}
